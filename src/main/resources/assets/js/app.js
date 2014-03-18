6ba2c3979069d058f4456fc19da9b18b3d96d314
Andrej Skoda <andrejskoda@gmail.com>
2014-03-18 20:33:02 +0100

    routeProvider

--- src/main/resources/assets/js/app.js
+++ src/main/resources/assets/js/app.js
@@ -2,66 +2,15 @@
  * Created by andrej on 3/5/14.
  */
 'use strict';
-var app = angular.module('myApp',[]);
+var app = angular.module('myApp',['ngRoute']);
 
 
 app.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/assets/list', {
-        templateUrl: 'assets/list/layout.html',
-        controller: PersonController
-    });
-
-    $routeProvider.otherwise({redirectTo: '/assets/list'});
+	$routeProvider.
+		when('/list', {
+    		templateUrl: 'list/layout.html',
+    		controller: PersonController
+    	}).otherwise({
+    		redirectTo: '/list'
+    	});
 }]);
-
-/*
-app.controller('PersonController', function($scope, $http) {
-
-    $scope.filteredPeople = [];
-    $scope.people = [];
-    $scope.currentPage = 1;
-    $scope.numPerPage = 5;
-    $scope.maxSize = 5;
-    $scope.viewMode = false;
-
-    $scope.fetchArticlesList = function() {
-        $http.get('/people').success(function(peopleList){
-            $scope.people = peopleList;
-            $scope.filteredPeople = $scope.people.slice(0, $scope.numPerPage);
-
-        });
-    }
-
-    $scope.viewPerson = function(selectedPerson) {
-        $scope.resetError();
-        $scope.viewMode = true;
-        $http.get('/people/' + selectedPerson.id).success(function(person){
-            $scope.person = person;
-        });
-    }
-
-    $scope.numPages = function () {
-        return Math.ceil($scope.people.length / $scope.numPerPage);
-    };
-
-    $scope.$watch('currentPage + numPerPage', function() {
-        var begin = (($scope.currentPage - 1) * $scope.numPerPage)
-        , end = begin + $scope.numPerPage;
-
-        $scope.filteredPeople = $scope.people.slice(begin, end);
-    }); 
-
-    $scope.resetError = function() {
-        $scope.error = false;
-        $scope.errorMessage = '';
-    }
-
-    $scope.setError = function(message) {
-        $scope.error = true;
-        $scope.errorMessage = message;
-    }
-
-    $scope.fetchArticlesList();
-    $scope.predicate = 'id';
-});
-*/
